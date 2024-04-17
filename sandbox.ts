065b767af2e5ec6a21b23da77c39928cb33dc17a
Mohammad Atikur Rhaman <atikur.rhaman@enosisbd.com>
2024-04-17 12:12:58 +0600

    Initial commit

---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const firstName = "Atikur";
+console.log(firstName);
+
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input);    
+});
